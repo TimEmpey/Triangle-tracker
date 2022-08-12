144d3e2c927d670e02334a0dbc331f76a41a3b43
Tim Empey <timothyempey@gmail.com>
2022-08-11 23:07:14 -0700

    add if else statement to filter through user input and triangle possibilities

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
 let triangleAnswer = function triangleChecker(){
-    let num1 = parseInt.document.getElementById("num1Input").value;
-    console.log("num1");
-    let num2 = parseInt.document.getElementById("num2Input").value;
-    console.log("num2");
-    let num3 = parseInt.document.getElementById("num3Input").value;
-    console.log("num3");
+    let num1 = parseInt(document.getElementById("num1Input")).value;
+    let num2 = parseInt(document.getElementById("num2Input")).value;
+    let num3 = parseInt(document.getElementById("num3Input")).value;
+
+let triType;
+if (num1 === num2 && num1 === num3 && num2 === num3){
+    triType = "Equal";
+    console.log("Equal");
+  } else if (num1 === num2 || num1 === num3 || num2 === num3){
+    triType = "Isosceles";
+    console.log("Isosceles");
+  } else if (num1 + num2 <= num3 || num1 + num3 <= num2 || num2 + num3 <= num1){
+    triType = "Not a Triangle";
+    console.log("Not a Triangle");
+  } else (num1 != num2 && num1 != num && num2 != num3);{
+    triType ="Scalene";
+    console.log("Scalene");
+  };
+
+  document.getElementById("output").innertext = triType;
 
 };
 
 window.addEventListener("load", function() {
-  const form = document.getElementById("form")
+  const form = document.getElementById("userInput");
   form.addEventListener("submit", submitInput);
 });
 
 function submitInput(event) {
   event.preventDefault();
   triangleAnswer();
-}
+};
